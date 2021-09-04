5e3caf1fa5d60fab5cb2b504e7db1bbe4b1f43e6
hagitbi94 <hagitbi94@gmail.com>
2021-09-04 21:54:10 +0300

    c

--- src/main/java/com/catalog/controllers/ProductController.java
+++ src/main/java/com/catalog/controllers/ProductController.java
@@ -20,7 +20,7 @@ import io.swagger.annotations.Api;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://https://open-l-project.herokuapp.com:4200")
 @RequestMapping("/product")
 public class ProductController {
 
